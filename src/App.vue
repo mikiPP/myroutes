d09570edaa7d6e840d964b1be15fb546fc5201b3
unknown <ocelot269@gmail.com>
2020-02-10 18:30:43 +0100

    feat(create new navbar todoo): new navbar in construction

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,28 @@
 <template>
-  <div>
-    <navbar></navbar>
-    <div>
-      <small>Flat</small>
-      <md-button>Default</md-button>
-      <md-button :md-ripple="false">Ripple Off</md-button>
-      <md-button class="md-primary">Primary</md-button>
-      <md-button class="md-accent">Accent</md-button>
-      <md-button disabled>Disabled</md-button>
-    </div>
-  <router-view></router-view>
-    <div>
-      <small>Raised</small>
-      <md-button class="md-raised">Default</md-button>
-      <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
-      <md-button class="md-raised md-primary">Primary</md-button>
-      <md-button class="md-raised md-accent">Accent</md-button>
-      <md-button class="md-raised" disabled>Disabled</md-button>
-    </div>
-
-    <div>
-      <small>Dense</small>
-      <md-button class="md-dense md-primary">Flat</md-button>
-      <md-button class="md-dense md-raised md-primary">Raised</md-button>
-    </div>
+<navbar></navbar>
+  <!-- <div id="app">
   </div>
+   -->
 </template>
+
 <script>
-import navbar from "./components/navbar";
+import navbar from './components/navbar'
+
 export default {
-  components: { navbar }
-};
+  name: 'App',
+  components: {
+    navbar
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
