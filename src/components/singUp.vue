a4a531bf35c1764bd94bd1b392ada7d8f07e6f20
ocelot269 <ocelot269@gmail.com>
2020-02-25 22:36:33 +0100

    añadiendo validaciones al formulario

--- src/components/singUp.vue
+++ src/components/singUp.vue
@@ -1,70 +1,179 @@
 <template>
-  <form id="app">
-    <h1>{{ msg }}hola{{ sayHello() }}</h1>
-    <label for="nombre">nombre
-      <input type="text" name="nombre" placeholder="nombre" v-model="nombre" />
-    </label>
-    <label for="apellidos">apellidos
-      <input type="text" placeholder="apellidos" name="apellidos" v-model="apellidos" />
-    </label>
-    <label for="edad">edad
-      <input type="number" name="edad" max="115" min="1" placeholder="11" v-model.number="edad"/>
-    </label>
+  <div id="app">
+    <nav>
+      Myroutes
+    </nav>
+    <b-form id="form" @submit="onSubmit" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Introduce tu email"
+        ></b-form-input>
+      </b-form-group>
 
-    <!--checkbox-->
-    <div id="checkbox">
-      <label for="genero">genero</label>
-      
-      <label for="hombre">
-        <input type="radio" v-model="genero[0]"/>
-        <span></span>
-      hombre
-      </label>
+      <b-form-group id="input-group-2" label="Tu nombre:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Miki mono"
+        ></b-form-input>
+      </b-form-group>
 
-      <label for="mujer">
-        <input type="radio" />
-        <span></span>
-        mujer
-      </label>
-    </div>
+      <b-form-group id="input-group-3" label="Tu edad:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.edad"
+          type="number"
+          :state="validAge()"
+          required
+          placeholder="Tu edad"
+          min="1"
+          max="100"
+        ></b-form-input>
+      </b-form-group>
 
-    <label for="fecha nacimiento"
-      >cumpleaños
-      <input type="date" name="cumpleaños" placeholder="1/10/1990" v-model="fechaNacimieto"/>
-    </label>
-    
-    <label for="email"
-      >cumpleaños
-      <input type="email" name="email" placeholder="a@gmail.com" v-model="email"/>
-    </label>
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col>
+            <b-form-radio-group label="Genero" v-model="form.genero" id="genero">
+              <b-form-radio name="radio" value="Hombre">Hombre</b-form-radio>
+              <b-form-radio name="radio" value="Mujer">Mujer</b-form-radio>
+            </b-form-radio-group></b-col
+          >
+          <b-col> <b-form-input v-model="form.fechaNacimiento" :state="validDate()" type="date" placeholder="Enter your name">
+          </b-form-input>
+          <!--mensaje de error-->
+           <b-form-invalid-feedback id="input-live-feedback">{{mensajeError("año",this.añoIntroducido,this.añoActual)}}</b-form-invalid-feedback>
+    <div class="mt-2">Value: {{ form.fechaNacimiento }}</div></b-col>
+        </b-row>
+      </b-container>
 
-    <label class="container">
-      <input type="checkbox"  />
-      <span></span>
-    politicas</label>
-    <button type="submit" value="enviar">registrar</button>
-  </form>
+      <b-form-checkbox
+        id="checkbox-1"
+        v-model="form.terminos"
+        name="checkbox-1"
+        value="true"
+        unchecked-value="false">
+        Acepto los terminos y condiciones
+      </b-form-checkbox>
+
+      <div>
+        State: <strong>{{ form.terminos }}</strong>
+      </div>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
 </template>
 
 <script>
-
 export default {
-  element: "#app",
-  data: function() {
-    return { msg: "hola",
-    fechaNacimieto:null,
-    nombre:null,
-    apellidos:null,
-    edad:null,
-    email:null,
-    genero:[false,false]};
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        edad: null,
+        genero: "",
+        terminos: false,
+        fechaNacimiento:null
+      },
+      show: true,
+      añoIntroducido:null,
+      añoActual:new Date().getFullYear()
+    };
   },
   methods: {
-    sayHello: function() {
-      return "hello";
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },mensajeError(datoReferencia,variable,datoAdicional=""){
+      let mensaje = "El "+ datoReferencia + " introducido  " + variable;
+      mensaje += variable < 1900 ? " es inferior " : " es superior ";
+      mensaje += "al " + datoReferencia + " actual " + datoAdicional +" porfavor cambie el " + datoReferencia;
+      return mensaje;
+    },
+    validDate(){
+      let resultado = false;
+      if (this.form.fechaNacimiento) {
+        //localizar guion
+        let indexGuion = this.form.fechaNacimiento.indexOf("-");
+        this.añoIntroducido = this.form.fechaNacimiento.substring(0,indexGuion);
+        //comprueba que tenga una fecha valida
+        if (this.añoIntroducido > 1900  && this.añoIntroducido < this.añoActual -8){ 
+          resultado = true;
+        }
+      return resultado
+      }
+    },
+    validAge(){
+      if (this.form.edad > 0 && this.form.edad < 110 && !this.form.edad.includes("e")) {
+        this.form.edad = Math.round(this.form.edad);
+        return true 
+      }
+      return false
     }
+
+    // onReset(evt) {
+    //   evt.preventDefault();
+    //   // Reset our form values
+    //   this.form.email = "";
+    //   this.form.name = "";
+    //   this.form.food = null;
+    //   this.form.checked = [];
+    //   // Trick to reset/clear native browser form validation state
+    //   this.show = false;
+    //   this.$nextTick(() => {
+    //     this.show = true;
+    //   });
+    // }
+  },
+  mounted: function() {
+    document.getElementById("genero").addEventListener("click", evento => {
+      if (!evento.target.innerHTML) {
+        this.form.genero = evento.target.value;
+      }
+    });
+    document.getElementById("checkbox-1").addEventListener("click", evento => {
+      this.form.terminos = !evento.target.value;
+    });
   }
 };
 </script>
 
-<style></style>
+<style>
+div {
+  width: auto;
+  padding: 1px;
+  margin: 1px;
+}
+nav {
+  margin: 0 !important;
+  padding: 0 !important;
+
+  position: relative;
+  bottom: 1.5em;
+  width: 100%;
+  height: 100px;
+  background-color: #3f51b5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  color: white;
+  font-size: 2.5em;
+  padding: 1.5em;
+}
+</style>
